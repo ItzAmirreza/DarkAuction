1908458980ee15f8e0677cdfe880c90eb3bd0e20
Dead_Light <amirrezawt@gmail.com>
2020-07-21 17:21:41 +0400

    Fully completed version

--- src/main/java/me/gamepixel/sbdarkauction/guilist/FirstGui.java
+++ src/main/java/me/gamepixel/sbdarkauction/guilist/FirstGui.java
@@ -6,7 +6,6 @@ import me.gamepixel.sbdarkauction.tasks.StartingDA;
 import me.mattstudios.mfgui.gui.guis.Gui;
 import me.mattstudios.mfgui.gui.guis.GuiItem;
 import org.bukkit.*;
-import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -18,6 +17,10 @@ import java.util.List;
 
 public class FirstGui {
 
+    //phase 1 --> gold nugget
+    //phase 2 --> gold ingot
+    //phase 3 --> emerald ingot
+    //phase 4 --> Diamond
     public static Gui gui = new Gui(SBDarkAuction.getInstance(), 6, Utils.color("&6Bid Page"));
     public static GuiItem graystainedglass = new GuiItem(new ItemStack(Material.STAINED_GLASS_PANE, 1, (short) 7));
     //red border
@@ -30,29 +33,61 @@ public class FirstGui {
 
     public static void makeguipage() {
 
+        if (!Utils.guiphase.containsKey("phase")) {
+
+            Utils.guiphase.put("phase", 1);
+
+        }
+
         GuiItem sellingitem = new GuiItem(Utils.itemRightNow);
         //setting the main item which is going to be sold
         gui.setItem(22, sellingitem);
         //borders
         gui.getFiller().fillBorder(redstainedglass);
         for (int i : biddingItemsSlot()) {
-            ItemStack goldnug = new ItemStack(Material.GOLD_NUGGET, findRightAmount(i));
-            int initialamount = 0;
-            int amount = 0;
-            while (initialamount != findRightAmount(i)) {
-                amount = amount + 150;
-                initialamount++;
-            }
-            ItemMeta metai = goldnug.getItemMeta();
-            metai.setDisplayName(Utils.color("&e" + Integer.toString(amount) + " &eCoins ◈"));
-            goldnug.setItemMeta(metai);
+            int amount = findRightPrice(i);
             ItemStack barr = new ItemStack(Material.BARRIER, findRightAmount(i));
             ItemMeta metai2 = barr.getItemMeta();
             metai2.setDisplayName(Utils.color("&c" + Integer.toString(amount) + " &cCoins ◈"));
             barr.setItemMeta(metai2);
-            GuiItem goldnuggets = new GuiItem(goldnug);
             GuiItem barrier = new GuiItem(barr);
-            gui.setItem(i, goldnuggets);
+            if (Utils.guiphase.get("phase") == 1) {
+                ItemStack goldnug = new ItemStack(Material.GOLD_NUGGET, findRightAmount(i));
+                ItemMeta metai = goldnug.getItemMeta();
+                metai.setDisplayName(Utils.color("&e" + Integer.toString(amount) + " &eCoins ◈"));
+                goldnug.setItemMeta(metai);
+                GuiItem goldnuggets = new GuiItem(goldnug);
+                gui.setItem(i, goldnuggets);
+            } else if (Utils.guiphase.get("phase") == 2) {
+                ItemStack goldnug = new ItemStack(Material.GOLD_INGOT, findRightAmount(i));
+                ItemMeta metai = goldnug.getItemMeta();
+                metai.setDisplayName(Utils.color("&e" + Integer.toString(amount) + " &eCoins ◈"));
+                goldnug.setItemMeta(metai);
+                GuiItem goldnuggets = new GuiItem(goldnug);
+                gui.setItem(i, goldnuggets);
+
+
+            } else if (Utils.guiphase.get("phase") == 3) {
+
+                ItemStack goldnug = new ItemStack(Material.EMERALD, findRightAmount(i));
+                ItemMeta metai = goldnug.getItemMeta();
+                metai.setDisplayName(Utils.color("&e" + Integer.toString(amount) + " &eCoins ◈"));
+                goldnug.setItemMeta(metai);
+                GuiItem goldnuggets = new GuiItem(goldnug);
+                gui.setItem(i, goldnuggets);
+
+            } else if (Utils.guiphase.get("phase") == 4) {
+                ItemStack goldnug = new ItemStack(Material.DIAMOND, findRightAmount(i));
+                ItemMeta metai = goldnug.getItemMeta();
+                metai.setDisplayName(Utils.color("&e" + Integer.toString(amount) + " &eCoins ◈"));
+                goldnug.setItemMeta(metai);
+                GuiItem goldnuggets = new GuiItem(goldnug);
+                gui.setItem(i, goldnuggets);
+
+
+            }
+
+
             gui.addSlotAction(i, event -> {
                 // Handle your open action
                 if (event.getCurrentItem().getType() == Material.BARRIER) {
@@ -62,24 +97,61 @@ public class FirstGui {
                     Player player = (Player) event.getWhoClicked();
                     player.playNote(player.getLocation(), Instrument.BASS_DRUM, Note.natural(1, Note.Tone.E));
 
-                } else if (event.getCurrentItem().getType() == Material.GOLD_NUGGET) {
+                } else if (event.getCurrentItem().getType() == Material.GOLD_NUGGET || event.getCurrentItem().getType() == Material.DIAMOND || event.getCurrentItem().getType() == Material.GOLD_INGOT || event.getCurrentItem().getType() == Material.EMERALD) {
                     event.setCancelled(true);
                     if (topbidplayer.get("top") == null || !topbidplayer.get("top").equals(event.getWhoClicked().getName())) {
-                        topbid.put(event.getWhoClicked().getName(), Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", "")));
-                        int biddedamount = Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", ""));
-                        topbidplayer.put("top", event.getWhoClicked().getName());
-                        gui.updateItem(i, getPlayerHead(event.getWhoClicked().getName(), findRightAmount(i)));
-                        StartingDA.countnum = 10;
-                        Player player = (Player) event.getWhoClicked();
-                        StartingDA.spawnBiddingHolo(biddedamount, player.getName());
-                        player.playSound(player.getLocation(), Sound.BLOCK_METAL_PLACE, 1.0F, 1.0F);
-                        ChangeToBarrierSlot(findPreviousBids(findRightAmount(i), player));
-                        for (Player player1 : StartingDA.playersinauction) {
 
-                            player1.sendMessage(Utils.color("&e" + player.getName() + " &7Has bidded &c" + Integer.toString(biddedamount) + "&e Coins ◈&7!"));
-                            player1.playSound(player1.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, 1.5F);
+                        if (isNextPageBid(event.getCurrentItem().getAmount())) {
+
+                            topbid.put(event.getWhoClicked().getName(), Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", "")));
+                            int biddedamount = Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", ""));
+                            topbidplayer.put("top", event.getWhoClicked().getName());
+                            gui.updateItem(i, getPlayerHead(event.getWhoClicked().getName(), findRightAmount(i)));
+                            StartingDA.countnum = 10;
+                            Player player = (Player) event.getWhoClicked();
+                            StartingDA.spawnBiddingHolo(biddedamount, player.getName());
+                            player.playSound(player.getLocation(), Sound.BLOCK_METAL_PLACE, 1.0F, 1.0F);
+                            ChangeToBarrierSlot(findPreviousBids(findRightAmount(i), player));
+                            for (Player player1 : StartingDA.playersinauction) {
+
+                                player1.sendMessage(Utils.color("&e" + player.getName() + " &7Has bidded &c" + Integer.toString(biddedamount) + "&e Coins ◈&7!"));
+                                player1.playSound(player1.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, 1.5F);
+
+                            }
+                            Bukkit.getScheduler().scheduleSyncDelayedTask(SBDarkAuction.getInstance(), new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (!(Utils.guiphase.get("phase") + 1 > 4)) {
+                                        Utils.guiphase.put("phase", Utils.guiphase.get("phase") + 1);
+                                        makeguipage();
+                                        try {
+                                            gui.update();
+                                        } catch (Exception e) {
+                                            //do noth
+                                        }
+                                    }
+                                }
+                            }, 20 * 2);
+                        } else {
+
+                            topbid.put(event.getWhoClicked().getName(), Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", "")));
+                            int biddedamount = Integer.parseInt(ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).replace("Coins", "").replace("◈", "").replace(" ", ""));
+                            topbidplayer.put("top", event.getWhoClicked().getName());
+                            gui.updateItem(i, getPlayerHead(event.getWhoClicked().getName(), findRightAmount(i)));
+                            StartingDA.countnum = 10;
+                            Player player = (Player) event.getWhoClicked();
+                            StartingDA.spawnBiddingHolo(biddedamount, player.getName());
+                            player.playSound(player.getLocation(), Sound.BLOCK_METAL_PLACE, 1.0F, 1.0F);
+                            ChangeToBarrierSlot(findPreviousBids(findRightAmount(i), player));
+                            for (Player player1 : StartingDA.playersinauction) {
+
+                                player1.sendMessage(Utils.color("&e" + player.getName() + " &7Has bidded &c" + Integer.toString(biddedamount) + "&e Coins ◈&7!"));
+                                player1.playSound(player1.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 1.0F, 1.5F);
+
+                            }
 
                         }
+
                     } else if (topbidplayer.get("top").equals(event.getWhoClicked().getName())) {
                         Player player = (Player) event.getWhoClicked();
                         player.sendMessage(Utils.color("&cYou are already the top bidder!"));
@@ -96,19 +168,14 @@ public class FirstGui {
             event.setCancelled(true);
         });
 
-        gui.getFiller().fill(redstainedglass);
+        gui.getFiller().fill(graystainedglass);
         
     }
 
     public static void ChangeToBarrierSlot(List<Integer> list) {
 
         for (int i : list) {
-            int initialamount = 0;
-            int amount = 0;
-            while (initialamount != findRightAmount(i)) {
-                amount = amount + 150;
-                initialamount++;
-            }
+            int amount = findRightPrice(i);
             ItemStack barr = new ItemStack(Material.BARRIER, findRightAmount(i));
             ItemMeta metai2 = barr.getItemMeta();
             metai2.setDisplayName(Utils.color("&c" + Integer.toString(amount) + " &cCoins ◈"));
@@ -124,7 +191,8 @@ public class FirstGui {
         ItemStack playerhead = getPlayerHead(player.getName(), num).getItemStack();
         List<Integer> barrierslots = new ArrayList<>();
         for (int i : biddingItemsSlot()) {
-            if (gui.getGuiItem(i).getItemStack().getType() == Material.GOLD_NUGGET || gui.getGuiItem(i).getItemStack() == playerhead) {
+            Material matt = gui.getGuiItem(i).getItemStack().getType();
+            if (matt == Material.GOLD_NUGGET || matt == Material.DIAMOND || matt == Material.EMERALD || matt == Material.GOLD_INGOT) {
                 int rightamount = findRightAmount(i);
                 if (rightamount < num) {
 
@@ -139,57 +207,65 @@ public class FirstGui {
 
     public static int findRightAmount(int i) {
         //10-11-12-14-15-16-19-21-23-25-28-30-32-34-36-37-39-40-41-43-44
+        int phasenumber = Utils.guiphase.get("phase");
         switch (i) {
             case 10:
-                return 5;
+                return 5+ ((phasenumber - 1 ) * 21);
             case 11:
-                return 6;
+                return 6+ ((phasenumber - 1 ) * 21);
             case 12:
-                return 7;
+                return 7+ ((phasenumber - 1 ) * 21);
             case 14:
-                return 15;
+                return 15+ ((phasenumber - 1 ) * 21);
             case 15:
-                return 16;
+                return 16+ ((phasenumber - 1 ) * 21);
             case 16:
-                return 17;
+                return 17+ ((phasenumber - 1 ) * 21);
             case 19:
-                return 4;
+                return 4+ ((phasenumber - 1 ) * 21);
             case 21:
-                return 8;
+                return 8+ ((phasenumber - 1 ) * 21);
             case 23:
-                return 14;
+                return 14+ ((phasenumber - 1 ) * 21);
             case 25:
-                return 18;
+                return 18+ ((phasenumber - 1 ) * 21);
             case 28:
-                return 3;
+                return 3+ ((phasenumber - 1 ) * 21);
             case 30:
-                return 9;
+                return 9+ ((phasenumber - 1 ) * 21);
             case 32:
-                return 13;
+                return 13+ ((phasenumber - 1 ) * 21);
             case 34:
-                return 19;
+                return 19+ ((phasenumber - 1 ) * 21);
             case 36:
-                return 1;
+                return 1 + ((phasenumber - 1 ) * 21);
             case 37:
-                return 2;
+                return 2+ ((phasenumber - 1 ) * 21);
             case 39:
-                return 10;
+                return 10+ ((phasenumber - 1 ) * 21);
             case 40:
-                return 11;
+                return 11+ ((phasenumber - 1 ) * 21);
             case 41:
-                return 12;
+                return 12+ ((phasenumber - 1 ) * 21);
             case 43:
-                return 20;
+                return 20+ ((phasenumber - 1 ) * 21);
             case 44:
-                return 21;
+                return 21+ ((phasenumber - 1 ) * 21);
             default:
                 return 100;
         }
     }
 
-    public int findRightSlot(int i) {
+
+    //fix this error later
+    public static int findRightSlot(int i) {
+
+        //22 --> 1
+
         //10-11-12-14-15-16-19-21-23-25-28-30-32-34-36-37-39-40-41-43-44
-        switch (i) {
+        int phasenumber = Utils.guiphase.get("phase");
+        int num = i - ((phasenumber - 1) * 21);
+        switch (num) {
             case 5:
                 return 10;
             case 6:
@@ -267,12 +343,7 @@ public class FirstGui {
     }
 
     public static GuiItem getPlayerHead(String name, int amountslot) {
-        int initialamount = 0;
-        int amount = 0;
-        while (initialamount != amountslot) {
-            amount = amount + 150;
-            initialamount++;
-        }
+        int amount = findRightPrice(findRightSlot(amountslot));
         ItemStack head = new ItemStack(Material.SKULL_ITEM, amountslot);
         head.setDurability((short) 3);
         SkullMeta meta = (SkullMeta) head.getItemMeta();
@@ -282,7 +353,28 @@ public class FirstGui {
         GuiItem skull = new GuiItem(head);
         return skull;
     }
-    
 
+    public static boolean isNextPageBid(int slotrealamount) {
+
+        if (slotrealamount%21 == 0) {
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+
+    public static int findRightPrice(int slot) {
+
+        int initialamount = 0;
+        int amount = 0;
+        while (initialamount != findRightAmount(slot)) {
+            amount = amount + 150;
+            initialamount++;
+        }
+        return amount;
+    }
 
 }
